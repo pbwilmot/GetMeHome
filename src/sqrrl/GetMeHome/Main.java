bfa707bca861c059a2b244519fdd07570e702770
peter wilmot <pmot199@gmail.com>
2011-09-17 10:15:40 -0400

    hello world

--- src/sqrrl/GetMeHome/Main.java
+++ src/sqrrl/GetMeHome/Main.java
@@ -2,12 +2,16 @@ package sqrrl.GetMeHome;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
     }
 }
