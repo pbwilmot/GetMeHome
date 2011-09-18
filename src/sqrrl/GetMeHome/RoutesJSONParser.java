db3ce2d9b306f59bd631146ba4bb41b73d314d58
peter wilmot <pmot199@gmail.com>
2011-09-17 21:26:50 -0400

    ready to put the front end on

---
+++ src/sqrrl/GetMeHome/RoutesJSONParser.java
@@ -0,0 +1,66 @@
+package sqrrl.GetMeHome;
+import org.json.*;
+public class RoutesJSONParser {
+	
+	private JSONObject json;
+	private String sjson;
+	private int statuscode,duration;
+	public RoutesJSONParser(String s)
+	{
+		sjson=s;
+	}
+	public boolean makeJSON()
+	{
+		try {
+			json= new JSONObject(sjson);
+			return true;
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean seedData()
+	{
+		try {
+			statuscode=json.getInt("statusCode");
+			if(statuscode!=200)
+			{
+				return false;
+			}
+			else
+			{
+				JSONArray resourcesets= json.getJSONArray("resourceSets");
+				JSONObject resourceset= resourcesets.getJSONObject(0);
+				JSONArray resources= resourceset.getJSONArray("resources");
+				JSONObject route= resources.getJSONObject(0);
+				
+				duration= route.getInt("travelDuration");
+				return true;
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			return false;
+		}
+
+	}
+	public int getStatusCode()
+	{
+		return statuscode;
+	}
+	public int getTravelDuration()
+	{
+		return duration; 
+	}
+	public String prettyJSON()
+	{
+		try {
+			return json.toString(4);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "{}";
+		}
+	}
+}
