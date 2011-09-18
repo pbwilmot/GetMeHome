6daac7343f53bfee790543acdbcc933d4f93d4c7
Patrick Temple <patrick_temple@brown.edu>
2011-09-17 22:33:25 -0400

    updated settings

--- gen/sqrrl/GetMeHome/R.java
+++ gen/sqrrl/GetMeHome/R.java
@@ -16,14 +16,21 @@ public final class R {
         public static final int walking=0x7f020002;
     }
     public static final class id {
-        public static final int Photo=0x7f050001;
-        public static final int PhotoLabel=0x7f050000;
+        public static final int Photo=0x7f060001;
+        public static final int PhotoLabel=0x7f060000;
+        public static final int editText1=0x7f060003;
+        public static final int linearLayout1=0x7f060002;
+        public static final int linearLayout2=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
