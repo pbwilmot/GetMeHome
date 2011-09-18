f8709e5a4e06d98190d2a59e5446b95a902dafb7
peter wilmot <pmot199@gmail.com>
2011-09-18 04:57:31 -0400

    jason woke up

--- src/sqrrl/GetMeHome/SettingsActivity.java
+++ src/sqrrl/GetMeHome/SettingsActivity.java
@@ -1,5 +1,7 @@
 package sqrrl.GetMeHome;
 
+import java.text.DecimalFormat;
+
 import android.content.*;
 import android.os.*;
 import android.view.View;
@@ -11,6 +13,8 @@ public class SettingsActivity extends MapActivity {
 
 	EditText address;
 	Button doneButton;
+	SeekBar seek;
+	TextView timeValueText;
 	
 	@Override
 	protected boolean isRouteDisplayed() {
@@ -22,7 +26,9 @@ public class SettingsActivity extends MapActivity {
 		super.onCreate(state);
 		setContentView(R.layout.settings);
 		doneButton = (Button)findViewById(R.id.done);
+		seek = (SeekBar)findViewById(R.id.timeValue);
 		address = (EditText)findViewById(R.id.addressText);
+		timeValueText = (TextView)findViewById(R.id.timeValueText);
 		doneButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -31,6 +37,26 @@ public class SettingsActivity extends MapActivity {
 				intent.putExtra("homeAddress", address.getText().toString());
 				setResult(RESULT_OK, intent);
 				finish();
+			}
+		});
+		seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				timeValueText.setText("$" + new DecimalFormat("#.##").format((float)progress / 100) + "/min");
 			}
 		});
 	}
